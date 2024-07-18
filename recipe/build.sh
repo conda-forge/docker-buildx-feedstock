8c40e8dbd588c2f139eb66a64d03bc005b75409c
danielnachun <daniel.nachun@gmail.com>
2024-07-18 11:35:49 -0700

    [ci skip] ***NO_CI*** enable other platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,8 +7,11 @@ go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" 
 mkdir -p ${PREFIX}/etc/bash_completion.d
 mkdir -p ${PREFIX}/share/zsh/site-functions
 mkdir -p ${PREFIX}/share/fish/vendor_completions.d
+
+if [[ ${build_platform} == ${target_platform} ]]; then
 docker-buildx completion bash > ${PREFIX}/etc/bash_completion.d/docker-buildx
 docker-buildx completion zsh > ${PREFIX}/share/zsh/site-functions/_docker-buildx
 docker-buildx completion fish > ${PREFIX}/share/fish/vendor_completions.d/docker-buildx.fish
+fi
 
 go-licenses save ./cmd/buildx --save_path=license-files
